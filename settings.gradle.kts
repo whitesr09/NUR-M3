28cb570f265edc4ef1178aaedafd6b0615b75ef5
whitesr09 <96966266+whitesr09@users.noreply.github.com>
2026-09-08 07:10:58 +0530

    build: initialize Android Gradle project

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "NUR-M3"
+include(":app")
