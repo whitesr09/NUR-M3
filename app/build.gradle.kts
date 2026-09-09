246518b4b92edcec3072951c670174cc21463166
whitesr09 <96966266+whitesr09@users.noreply.github.com>
2026-09-09 12:56:34 +0530

    build: prepare compatible 0.12.0 preview with versionCode 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.nshd.nurm3"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 5
+        versionName = "0.12.0-preview.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
