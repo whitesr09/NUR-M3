13ddf4af73087c471ea0e7450e4a520fbb63a6dc
whitesr09 <96966266+whitesr09@users.noreply.github.com>
2026-09-10 06:42:10 +0530

    chore(version): bump solid theme fix preview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.nshd.nurm3"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.27.0-preview.1"
+        versionCode = 7
+        versionName = "0.27.1-preview.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
